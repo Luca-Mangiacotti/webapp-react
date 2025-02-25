c269680602e1683f653e47f2eb6a02801ab37393
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-02-25 13:05:09 +0100

    feat: add detail page for movie

--- src/pages/Detailpage.jsx
+++ src/pages/Detailpage.jsx
@@ -1,1 +1,26 @@
-export default function Detailpage() {}
+import axios from "../api/axios";
+import { useParams, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+
+export default function Detailpage() {
+  const [movie, setMovie] = useState({});
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const fetchMovie = () => {
+    axios
+      .get(`/movies/${id}`)
+      .then((res) => {
+        setMovie(res.data);
+      })
+      .catch((err) => {
+        if (err.status === 404) {
+          navigate("/404");
+        }
+      });
+  };
+
+  useEffect(fetchMovie, [id, navigate]);
+
+  return <div>Pagina del film: {movie.title}</div>;
+}
